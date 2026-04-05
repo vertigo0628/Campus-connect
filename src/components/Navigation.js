c0a08e5ea06056d9a73bfa1a04299cbe3d2a081f
vertigo <vertigopoiuylkjhg@gmail.com>
2026-04-05 13:34:41 +0000

    fix: add missing React imports to Navigation for build stability

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,5 +1,7 @@
 "use client";
 
+import { useState, useEffect } from "react";
+
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import styles from "./Navigation.module.css";
