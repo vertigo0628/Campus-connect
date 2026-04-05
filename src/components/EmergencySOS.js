920612209c2e5f00116184fb0f4bd550fef93d7c
vertigo <vertigopoiuylkjhg@gmail.com>
2026-04-05 13:43:25 +0000

    fix: add missing useEffect import to EmergencySOS

--- src/components/EmergencySOS.js
+++ src/components/EmergencySOS.js
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { usePathname } from "next/navigation";
 import { db } from "@/lib/firebase";
 import { collection, addDoc, serverTimestamp } from "firebase/firestore";
